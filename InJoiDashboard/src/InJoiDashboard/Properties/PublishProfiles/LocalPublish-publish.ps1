965ed74a48a0e5d13ac3889cfdc2945b0a82db13
TohLengWee <wee0206@gmail.com>
2016-06-30 16:59:40 +0800

    Add publish file

---
+++ InJoiDashboard/src/InJoiDashboard/Properties/PublishProfiles/LocalPublish-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'fee02a48-d63d-4f55-9764-9a6748c36b43'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
---
+++ InJoiDashboard/src/InJoiDashboard/publish.ps1
@@ -0,0 +1,1 @@
+dotnet publish --framework netcoreapp1.0 --output "d:\wwwroot\InJoiDashboar" --configuration Release
